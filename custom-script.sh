c997a184d8ccceba04b3362d13f28c9894a834ec
pmohan14 <109452883+pmohan14@users.noreply.github.com>
2022-08-23 03:46:18 +0530

    Update custom-script.sh

--- custom-script.sh
+++ custom-script.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-${azure_buildprops_MAVEN_EXEC} -B -s ${azure_buildprops_MAVEN_SETTINGS} -Dsettings.security=${azure_buildprops_MAVEN_SECURITY_SETTINGS} $@ 2>&1 | tee
-${azure_buildprops_CICD_DIR}/mvn.log
+${azure_buildprops_MAVEN_EXEC} -B -s ${azure_buildprops_MAVEN_SETTINGS} -Dsettings.security=${azure_buildprops_MAVEN_SECURITY_SETTINGS} $@ 2>&1 | tee ${azure_buildprops_CICD_DIR}/mvn.log
 rc=$?; echo "exit code $rc"; if [[ $rc != 0 ]]; then ( echo 'could not perform build'; exit $rc; ) fi
 if [ -d ${azure_build_working_directory}/target/checkout ]; then
    cd ${azure_build_working_directory}/target/checkout
 else
    cd ${azure_build_working_directory}
 fi
-ls -la '**/*.war'
+ls -la target/*.jar
 if grep " BUILD FAILURE" ${azure_buildprops_CICD_DIR}/mvn.log
   then
   echo 'found errors in build log';
   exit 1;
 fi
-export artifactJar=$(basename **/*.war)
+export artifactJar=$(basename target/*.jar)
 export artifactJarSHA1=$(sha1sum target/${artifactJar} | sed -rn 's/(\w+)\W.*/\1/p')
 export artifactJarSHA256=$(sha256sum target/${artifactJar} | sed -rn 's/(\w+)\W.*/\1/p')
 export artifactId=$(${azure_buildprops_MAVEN_EXEC} org.apache.maven.plugins:maven-help-plugin:3.1.1:evaluate -Dexpression=project.artifactId -q -DforceStdout)
-export groupId=$(${azure_buildprops_MAVEN_EXEC} org.apache.maven.plugins:maven-help-plugin:3.1.1:evaluate -Dexpression=project.groupId -q -DforceStdout)
-${azure_buildprops_MAVEN_EXEC} -Dmaven.buildNumber.revisionOnScmFailure=NOREVISION -Dmaven.buildNumber.shortRevisionLength=7 buildnumber:create-metadata
+export groupId=$(${azure_buildprops_MAVEN_EXEC} org.apache.maven.plugins:maven-help-plugin:3.1.1:evaluate -Dexpression=project.groupId -q -DforceStdout) ${azure_buildprops_MAVEN_EXEC} -Dmaven.buildNumber.revisionOnScmFailure=NOREVISION -Dmaven.buildNumber.shortRevisionLength=7 buildnumber:create-metadata
 cat target/generated/build-metadata/build.properties | egrep -v "^[#|name|timestamp]" | sed 's/^/artifact\.build\./' >${azure_buildprops_CICD_DIR}/deploy.properties
 echo "artifact.file=${artifactJar}" >>${azure_buildprops_CICD_DIR}/deploy.properties
 echo "artifact.sha1=${artifactJarSHA1}" >>${azure_buildprops_CICD_DIR}/deploy.properties
